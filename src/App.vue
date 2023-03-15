6a1463089315399b9c261daad75fd98b7b4d7b14
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-03-16 00:09:22 +0100

    app cleaned

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <main class="App">
+    <Home />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script>
+  
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<template>
+  <div>
+     Home
+  </div>
+</template>
+
+<style scoped>
+
+</style>
