80ea4232588862d650ebcbfd4abc45077cdac647
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-03-19 01:34:32 +0100

    repos perpage done

---
+++ src/components/Pagination.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default{
+    name: 'Pagination',
+    props: ['totalRepos'],
+    
+}
+</script>
+
+<template>
+  <div class="paginationWrapper">
+  </div>
+</template>
+
+<style>
+
+</style>
